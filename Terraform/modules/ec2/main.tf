a7d4bbb2d9d13d7291188796917730856d6cc0d6
testuser <testuser@gmail.com>
2023-11-08 18:37:41 +0530

    update

--- Terraform/modules/ec2/main.tf
+++ Terraform/modules/ec2/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web" {
   instance_type               = "t2.micro"
   key_name                    = "docker"
   vpc_security_group_ids      = [var.sg_id]
-  subnet_id                   = var.subnets
+  subnet_id                   = var.subnets[count.index]
   availability_zone = data.aws_availability_zones.available.names
 
   root_block_device {
