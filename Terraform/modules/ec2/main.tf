045b4feb1ae565ee4bb9fa5ffb6e44d56f8251e0
testuser <testuser@gmail.com>
2023-11-08 18:42:20 +0530

    changes in ec2 modules

--- Terraform/modules/ec2/main.tf
+++ Terraform/modules/ec2/main.tf
@@ -1,11 +1,10 @@
 resource "aws_instance" "web" {
+  count                       = 1
   ami                         = data.aws_ami.amazon-2.id
   instance_type               = "t2.micro"
   key_name                    = "docker"
   vpc_security_group_ids      = [var.sg_id]
   subnet_id                   = var.subnets[count.index]
-  availability_zone = data.aws_availability_zones.available.names
-
   root_block_device {
     volume_size = var.vol_size
   }
